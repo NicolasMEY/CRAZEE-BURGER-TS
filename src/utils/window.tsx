7e6c65ab8c6323a625020ac554faad397ec0b54d
nicolasmeyer <nsg.meyer@hotmail.fr>
2024-12-06 22:44:30 +0100

    ♻️ TSE-03 : UTILS, Add window.tsx, vi's version

--- src/utils/window.tsx
+++ src/utils/window.tsx
@@ -1,14 +1,35 @@
 export const refreshPage = () => window.location.reload()
 
-type LocalStorageValue = string | number | boolean | object;
+// type LocalStorageValue = string | number | boolean | object;
 
-export const setLocalStorage = (key : string, value: LocalStorageValue) => {
+// export const setLocalStorage = (key : string, value: LocalStorageValue) => {
+//   localStorage.setItem(key, JSON.stringify(value))
+// }
+
+
+// <T,> Rajoute une sécurité contraitement à unknown, quand on ne sait pas ce que l'on va recevoir, si on ne connait pas la valeur on bouge vers un type générique , puis unknown, puis any...
+export const setLocalStorage = <T,> (key : string, value: T) => {
   localStorage.setItem(key, JSON.stringify(value))
 }
 
-export const getLocalStorage = (key: string): string | null => {
-  const value = localStorage.getItem(key);
-  return value ? JSON.parse(value) : null; // Retourne null si la valeur est absente
+export const getLocalStorage =  (key: string): unknown | null => {
+  const item = localStorage.getItem(key)
+  return item ? JSON.parse(item) : null
 }
 
-// null si la clé n'existe pas dans le stockage local. Cependant, la fonction JSON.parse() attend une chaîne de caractères en paramètre et ne peut pas gérer null.
+// **** Equivalent
+// export const getLocalStorage =  (key: string): unknown => {
+//   const item = localStorage.getItem(key);
+//   if (item)
+//   return JSON.parse(item) 
+// }
+
+// **** Equivalent
+// export const getLocalStorage = <T,> (key: string) => {
+//   const item = localStorage.getItem(key);
+//   if (item)
+//   return JSON.parse(item) as T 
+// }
+// CASTING : Typage à la volée, typer au moment ou la fonction va s'exécuter, récupérer ds informations, grace à as T (réassignation de type)
+// T et as T rajoute une couche de sécurité
+
